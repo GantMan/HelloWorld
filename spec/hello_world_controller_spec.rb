dd75a96e99310a8cae1b1301893a957ca5e341f1
Gant <GantMan@gmail.com>
2013-04-02 13:39:17 -0500

    Fixed small typo

--- spec/hello_world_controller_spec.rb
+++ spec/hello_world_controller_spec.rb
@@ -5,7 +5,7 @@ describe "HelloWorldController" do
     @rc = @window.rootViewController
 	end
 
-	it "should have have a UILabel with text of 'Hello World'" do
+	it "should have a UILabel with text of 'Hello World'" do
 		label = @rc.instance_variable_get("@label")
 		# check text
 		label.text.should.equal "Hello World"
