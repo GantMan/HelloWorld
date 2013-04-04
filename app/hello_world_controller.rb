ce8543665f8a19e8f8b342ef31dd2ed6b14144b7
Gant <GantMan@gmail.com>
2013-04-03 19:31:18 -0500

    Centering the hello world text

--- app/hello_world_controller.rb
+++ app/hello_world_controller.rb
@@ -3,10 +3,14 @@ class HelloWorldController < UIViewController
 		p "Hello World"
 
 		view.backgroundColor = UIColor.whiteColor
+		center_x = view.frame.size.width / 2
+		center_y = view.frame.size.height / 2
 
 		@label = UILabel.new
 		@label.text = "Hello World"
-		@label.frame = [[50,50], [150, 50]]
+		@label.sizeToFit
+		@label.center = [center_x, center_y]
+
 		view.addSubview(@label)
 	end
 end
