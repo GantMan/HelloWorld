5e416d702a32cb88bab9d2d3ee93482e23b11be4
Gant <GantMan@gmail.com>
2013-04-17 15:36:15 -0500

    Implementing SweatTea

--- app/styles/main_style.rb
+++ app/styles/main_style.rb
@@ -2,8 +2,7 @@ Teacup::Stylesheet.new :main do
 
 	style :hello_label,
 		text: "Hello World",
-		width: 89,
-		height: 21,
+		sizeToFit: true,
 		center: ['50%', '50%'],
-		backgroundColor: UIColor.clearColor
+		backgroundColor: :clear
 end
